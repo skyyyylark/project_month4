786224aef6b423e218143333b3431878808e3fd7
Magomedov Gazi-Magomed <85029421+skyyyylark@users.noreply.github.com>
2021-11-02 17:11:25 +0600

    Add files via upload

---
+++ main/admin.py
@@ -0,0 +1,10 @@
+from django.contrib import admin
+from .models import *
+
+# Register your models here.
+admin.site.register(News)
+admin.site.register(ImageNews)
+admin.site.register(Laws)
+admin.site.register(Publication)
+
+
